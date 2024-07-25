fd5b9b2891a6998ed5eacea85fd99c699e31424f
Samuel Cahyawijaya <samuel.cahyawijaya@gmail.com>
2024-07-25 16:32:52 +0800

    add metrics.ipynb

--- run_script/run-blind-gpt4.sh
+++ run_script/run-blind-gpt4.sh
@@ -1,4 +1,4 @@
-export OPENAI_API_KEY=cf016418d6b14bcb8165a11c97cf043e 
+export OPENAI_API_KEY=5580ab23f46e477a95ab455f361b5fdb 
 export OPENAI_AZURE_DEPLOYMENT=1 
 python open-eqa-predict.py --method blind-gpt4 --force --verbose
 python evaluate-predictions.py data/predictions/blind-gpt4-predictions.json --force --verbose
